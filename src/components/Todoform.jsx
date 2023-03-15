84ead31980ca44a564968fdc9f6be95a1fcf7c49
aryash19 <aryashchaudhary12341234@gmail.com>
2023-03-16 01:14:38 +0530

    new

--- src/components/Todoform.jsx
+++ src/components/Todoform.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import "../App.css";
 
 const Todoform = () => {
   return (
     <form className='form'>
     <input
-        placeholder="Enter new Todo"
+        placeholder="Enter new Todo...."
         className="input"
     />
       
